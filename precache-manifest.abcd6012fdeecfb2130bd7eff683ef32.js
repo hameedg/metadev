e0eec48142e8f749e1375f39f5c727722c0ec238
hameedg <adilsaberi01@gmail.com>
2022-06-27 16:40:38 +0530

    Updates

---
+++ precache-manifest.abcd6012fdeecfb2130bd7eff683ef32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "16a88548570188d5e013",
+    "url": "/react-gh-pages/static/css/main.0b5c345b.chunk.css"
+  },
+  {
+    "revision": "16a88548570188d5e013",
+    "url": "/react-gh-pages/static/js/main.16a88548.chunk.js"
+  },
+  {
+    "revision": "517527aa6758397229f0",
+    "url": "/react-gh-pages/static/js/1.517527aa.chunk.js"
+  },
+  {
+    "revision": "6a9b3959bee9cf60d492",
+    "url": "/react-gh-pages/static/js/2.6a9b3959.chunk.js"
+  },
+  {
+    "revision": "c1e660c4d2cafe861686",
+    "url": "/react-gh-pages/static/js/runtime~main.c1e660c4.js"
+  },
+  {
+    "revision": "0e0f3c9356f2e4f56a351ad1acd45a00",
+    "url": "/react-gh-pages/index.html"
+  }
+];
